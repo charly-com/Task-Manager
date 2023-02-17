67cee25af014c7e1227aa45cc8b8c8fec095e66c
Decagon <uchecharles223@gmail.com>
2023-02-17 03:03:54 +0100

    created a theme for the project using materia ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { FC, ReactElement } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './theme/customTheme';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-
-function App() {
-  return <h1>Hello world</h1>
-}
+const App: FC = (): ReactElement => {
+    return (
+        <ThemeProvider theme={customTheme}>
+            <CssBaseline />
+            <Dashboard />
+        </ThemeProvider>
+    );
+};
 
 export default App;
